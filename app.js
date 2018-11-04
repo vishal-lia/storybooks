033dfa19404b4f40cd038db0a02e184c562eb4fe
vishal-lia <vishallia77@gmail.com>
2018-11-04 21:44:16 +0530

    Google verification

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ require('./config');
 const express = require('express');
 const mongoose = require('mongoose');
 const passport = require('passport');
+const path = require('path');
 
 // Passport Config
 require('./config/passport')(passport);
@@ -12,6 +13,8 @@ const auth = require('./routes/auth');
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
     res.send('Welcome to StoryBooks!');
